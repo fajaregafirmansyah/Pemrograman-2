92a30ca44454883e8b12a2b19c1bc5cc49ad0aec
Fajar Ega Firmansyah <fajar@neuronworks.co.id>
2019-02-01 19:30:41 +0700

    fixing tugas 3

---
+++ tugas3/Mobil.java
@@ -0,0 +1,124 @@
+package tugas3;
+
+public class Mobil {
+
+    boolean cek;
+    int kecepatanActual;
+    int kecepatanMaksimum;
+    int percepatan;
+    int Gigi;
+
+    public Mobil(boolean cek, int kecepatanActual, int kecepatanMaksimum, int percepatan, int Gigi) {
+        this.cek = cek;
+        this.kecepatanActual = kecepatanActual;
+        this.kecepatanMaksimum = kecepatanMaksimum;
+        this.percepatan = percepatan;
+        this.Gigi = Gigi;
+    }
+
+    public boolean isCek() {
+        return cek;
+    }
+
+    public void setCek(boolean cek) {
+        this.cek = cek;
+    }
+
+    public int getKecepatanActual() {
+        return kecepatanActual;
+    }
+
+    public void setKecepatanActual(int kecepatanActual) {
+        this.kecepatanActual = kecepatanActual;
+    }
+
+    public int getKecepatanMaksimum() {
+        return kecepatanMaksimum;
+    }
+
+    public void setKecepatanMaksimum(int kecepatanMaksimum) {
+        this.kecepatanMaksimum = kecepatanMaksimum;
+    }
+
+    public int getPercepatan() {
+        return percepatan;
+    }
+
+    public void setPercepatan(int percepatan) {
+        this.percepatan = percepatan;
+    }
+
+    public int getGigi() {
+        return Gigi;
+    }
+
+    public void setGigi(int Gigi) {
+        this.Gigi = Gigi;
+    }
+
+    public void start() {
+        if (!this.isCek()) {
+            this.cek = true;
+            System.out.println("\nMobil Alphard sudah dinyalakan.");
+        } else {
+            this.cek = true;
+            System.out.println("\nMobil Alphard sudah dalam kondisi menyala.");
+        }
+    }
+
+    public void stop() {
+        if (this.isCek() && this.getKecepatanActual() == 0) {
+            this.cek = false;
+            System.out.println("\nMobil Alphard sudah dalam kondisi mati.");
+        } else if (this.isCek() && this.getKecepatanActual() != 0) {
+            System.out.println("\nSilahkan berhenti terlebih dahulu yaa mobilnya..");
+        } else {
+            System.out.println("\nSilahkan starter terlebih dahulu");
+        }
+    }
+
+    public void rem() {
+        this.setKecepatanActual(this.getKecepatanActual() / 2);
+        System.out.println("\nKecepatan mobil Alphard saat ini : " + this.getKecepatanActual() + " Km/jam");
+    }
+
+    public void gantiGigi(int Gigi) {
+        this.Gigi = Gigi;
+        switch (Gigi) {
+            case 0:
+                this.percepatan = 0;
+                break;
+            case 1:
+                this.percepatan = 10;
+                break;
+            case 2:
+                this.percepatan = 20;
+                break;
+            case 3:
+                this.percepatan = 30;
+                break;
+            case 4:
+                this.percepatan = 40;
+                break;
+            case 5:
+                this.percepatan = 50;
+                break;
+        }
+
+        System.out.println("Posisi Gigi mobil Alphard : " + this.getGigi());
+        System.out.println("\nPercepatan Mobil Alphard : " + this.getPercepatan() + " Km/jam^2");
+    }
+
+    public void gas() {
+        if (this.getKecepatanActual() < this.getKecepatanMaksimum()) {
+            if (this.getKecepatanMaksimum() < this.getKecepatanActual() + this.getPercepatan()) {
+                this.setKecepatanActual(this.getKecepatanMaksimum());
+            } else {
+                this.setKecepatanActual(this.getKecepatanActual() + this.getPercepatan());
+            }
+        } else {
+            System.out.println("\nMobil Alphard kecepatannya udah maksimum. Hati-hati yaa dalam mengemudi");
+        }
+        System.out.println("\nKecepatan mobil Alphard saat ini : " + this.getKecepatanActual() + " Km/jam");
+    }
+}
---
+++ tugas3/MobilActivity.java
@@ -0,0 +1,74 @@
+package tugas3;
+
+import java.util.Scanner;
+
+public class MobilActivity {
+
+    public static Boolean jalan = true;
+    private static int memilih;
+    private static int gigi;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Mobil aktivitasMobil = new Mobil(false, 0, 180, 5, 0);
+        aktivitasMobil.setKecepatanMaksimum(280);
+
+        while (jalan) {
+            System.out.println("====================================");
+            System.out.println("Selamat Datang Di Menu Mobil Alphard");
+            System.out.println("====================================");
+
+            System.out.println("1. Starter Mobil Alphard");
+            System.out.println("2. Stop Mobil Alphard");
+            System.out.println("3. Rem Mobil Alphard");
+            System.out.println("4. Ganti Gigi Mobil Alphard");
+            System.out.println("5. Gas Mobil Alphard");
+            System.out.println("6. Keluar Menu Mobil Alphard");
+
+            System.out.print("\nPilihan kamu : ");
+            try {
+                memilih = input.nextInt();
+            } catch (Exception e) {
+                memilih = 0;
+                jalan = false;
+            }
+
+            switch (memilih) {
+                case 1:
+                    aktivitasMobil.start();
+                    break;
+                case 2:
+                    aktivitasMobil.stop();
+                    break;
+                case 3:
+                    aktivitasMobil.rem();
+                    break;
+                case 4:
+                    System.out.print("Mobil Alphard Ganti ke gigi (1/2/3/4/5) : ");
+                    try {
+                        gigi = input.nextInt();
+                        if (gigi >= 0 && gigi <= 5) {
+                            aktivitasMobil.gantiGigi(gigi);
+                        }
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Ops, Kamu salah masukkan gigi. Masukkan gigi angka 1 sampai dengan 5 ya!");
+                    }
+                    break;
+                case 5:
+                    aktivitasMobil.gas();
+                    break;
+                case 6:
+                    jalan = false;
+                    break;
+                default:
+                    System.out.println("Ops, Kamu salah masukkan pilihan. Masukkan hanya angka 1 sampai dengan 6 yaa. Silahkan coba lagi!\n");
+                    break;
+
+            }
+
+        }
+
+    }
+
+}
